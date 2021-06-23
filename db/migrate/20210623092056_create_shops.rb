39c8489f7802cf4512eab4a29c57353f2a5e260b
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-06-23 18:26:58 +0900

    installed pundit

---
+++ db/migrate/20210623092056_create_shops.rb
@@ -0,0 +1,14 @@
+class CreateShops < ActiveRecord::Migration[6.0]
+  def change
+    create_table :shops do |t|
+      t.string :name
+      t.string :address
+      t.string :phone_number
+      t.string :website
+      t.string :images
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_23_050811) do
+ActiveRecord::Schema.define(version: 2021_06_23_092056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shops", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone_number"
+    t.string "website"
+    t.string "images"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_shops_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +39,5 @@ ActiveRecord::Schema.define(version: 2021_06_23_050811) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "shops", "users"
 end
